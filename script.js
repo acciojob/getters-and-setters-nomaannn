3a29cb50b8dd0636e35ee8aa64f069fc1ca07e8e
Nomaan Ahmad <94966523+nomaannn@users.noreply.github.com>
2023-04-24 08:53:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 //complete this code
-class Person {}
+class Person {
+	name;
+	age;
+	constructor(name,age){
+		// this.name = name
+		// this.age = age
+	}
+	get name(){
+		this.name = name
+	}
+	set age(){
+		this.age = age
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	 study(){
+		console.log(name)
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	teach(){
+		console.log(`${name}is teaching`)
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
